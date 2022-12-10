b84473a2448623dbdab93067bf3c7e3494da3481
modather <modather11.a@gmail.com>
2022-12-09 22:28:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return Aleays 0 (Seccss)
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0 ;
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+
 }
