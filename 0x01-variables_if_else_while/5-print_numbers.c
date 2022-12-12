51c81aa4631566e4fdff109e158c6a108642982c
modather <modather11.a@gmail.com>
2022-12-12 01:37:22 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,12 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		putchar(i);
+	for (i = 0; i <= 10; i++)
+		printf("%d", i);
 	putchar('\n');
 	return (0);
 
