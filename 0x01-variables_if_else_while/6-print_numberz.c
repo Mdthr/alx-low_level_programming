69c4cf51de082e6fdd8404e149c3f269a5ce3822
modather <modather11.a@gmail.com>
2022-12-12 02:22:39 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+
+}
