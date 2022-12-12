6f4e1f6433072856fbd392a5559c249ff431f891
modather <modather11.a@gmail.com>
2022-12-12 07:18:27 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Print alphabet
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+
+}
