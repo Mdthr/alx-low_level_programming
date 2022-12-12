a2c22ec911da56cd5c3ff66b1bf61ddf0adc1baa
modather <modather11.a@gmail.com>
2022-12-12 03:40:26 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	int a;
+
+	c = 'a';
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a);
+			a++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+
+}
