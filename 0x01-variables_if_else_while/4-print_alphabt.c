fd2e74ef08fd7e74ab607e04a488401fa7070a26
modather <modather11.a@gmail.com>
2022-12-12 00:10:48 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while
+		(x <= 'z') {
+			if ((x != 'q' && x != 'e') && x <= 'z')
+				putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+
+}
