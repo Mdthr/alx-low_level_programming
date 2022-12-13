892c97bd69174736604ea93abad65573265f702f
modather <modather11.a@gmail.com>
2022-12-13 14:44:40 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print Alphabtic
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+
+	_putchar('/n');
+	return (0);
+
+}
