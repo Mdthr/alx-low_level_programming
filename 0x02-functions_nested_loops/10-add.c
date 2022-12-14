8585ad795cfc6a9b9bde6d6dc2bdf78079d92cc2
modather <modather11.a@gmail.com>
2022-12-14 08:47:22 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add tow value
+ * @n1: first number
+ * @n2: second number
+ * Return: return integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-7-print_last_digit.c
+10-add.c
