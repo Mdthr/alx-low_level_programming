2992eeb7f02640eadffec20d23091b509fb1afa4
modather <modather11.a@gmail.com>
2022-12-15 15:31:59 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,13 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b > 9)
+			{
+			_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
