a5a09e7d66e4641157a8e7cc240ba35cdc88eb28
modather <modather11.a@gmail.com>
2022-12-19 08:01:43 -0800

    swap_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: interger to swap
+ * @b: integer to swap
+ */
+wap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0-reset_to_98.c
+void swap_int
